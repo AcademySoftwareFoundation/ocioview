060f511b1d06997126482aa2b6945f1a1e13ffd0
Michael Dolan <michdolan@gmail.com>
2026-06-05 22:45:48 -0400

    test: add reference model-tier tests for ColorSpaceModel
    Signed-off-by: Michael Dolan <michdolan@gmail.com>

---
+++ tests/models/test_color_space_model.py
@@ -0,0 +1,66 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenColorIO Project.
+
+import PyOpenColorIO as ocio
+
+from ocioview.items.color_space_model import ColorSpaceModel
+from ocioview.undo import undo_stack
+
+
+def test_create_item_adds_color_space_and_undo_restores():
+    config = ocio.GetCurrentConfig()
+    before = config.serialize()
+
+    model = ColorSpaceModel()
+    model.create_item("foo")
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is not None
+
+    undo_stack.undo()
+    assert ocio.GetCurrentConfig().serialize() == before
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is None
+
+
+def test_rename_via_set_data_and_undo_restores():
+    config = ocio.GetCurrentConfig()
+    model = ColorSpaceModel()
+    model.create_item("foo")
+    before = config.serialize()
+
+    index = model.get_index_from_item_name("foo")
+    name_index = index.sibling(index.row(), model.NAME.column)
+    model.setData(name_index, "bar")
+
+    assert ocio.GetCurrentConfig().getColorSpace("bar") is not None
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is None
+
+    undo_stack.undo()
+    assert ocio.GetCurrentConfig().serialize() == before
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is not None
+    assert ocio.GetCurrentConfig().getColorSpace("bar") is None
+
+
+def test_remove_unused_color_space():
+    config = ocio.GetCurrentConfig()
+    model = ColorSpaceModel()
+    model.create_item("foo")
+
+    row = model.get_index_from_item_name("foo").row()
+    model.removeRows(row, 1)
+
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is None
+
+
+def test_removal_guard_refuses_referenced_color_space():
+    config = ocio.GetCurrentConfig()
+    model = ColorSpaceModel()
+    model.create_item("foo")
+    ocio.GetCurrentConfig().setRole("my_role", "foo")
+
+    warnings = []
+    model.warning_raised.connect(warnings.append)
+
+    row = model.get_index_from_item_name("foo").row()
+    model.removeRows(row, 1)
+
+    assert ocio.GetCurrentConfig().getColorSpace("foo") is not None  # not removed
+    assert warnings  # a refusal warning was raised
